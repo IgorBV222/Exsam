8fca7bf00abde13ca253c9f18d7b7adcea646565
ADMIN <ADMIN@DESKTOP-CBRV7QC>
2024-01-10 21:23:12 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Exsam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 34);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(320, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Выберите папку для поиска файлов, содержащих слова";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(317, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите слова для поиска через пробел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Выбрана папка:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(424, 25);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(798, 394);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(424, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(274, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Информация о файлах, содержащих искомые слова";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 180);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(316, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Пауза";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(122, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Возобновить";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(247, 280);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Стоп";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1234, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exsam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();     
+        }
+        
+        private static Process[] processes;
+        Thread threadFile;
+
+        public string[] arr_words;
+        public List<string> listInfo = new List<string>();
+
+        //имя
+        public string nameFileInfo = string.Empty;
+        //размер
+        public long sizeFileInfo = 0;
+        //путь
+        public string pathFileInfo = string.Empty;
+        //количество совпадений  в файле
+        public int count = 0;
+
+        //Считываем из textbox слова в массив
+        private void WordsToSearch()
+        {
+            arr_words = textBox1.Text.Split();
+        }
+
+        // Определяем папку для поиска файлов
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        //Ищем все файлы .txt и совпадения в них
+        public void FindFile(string path)
+        {
+            Matcher matcher = new Matcher();
+            matcher.AddIncludePatterns(new[] { "**/*.txt" });
+            foreach (string file in matcher.GetResultsInFullPath(path))
+            {
+                int countConsilience = 0;
+                //Читаем файл
+                string tmp = File.ReadAllText(file);
+                //Ищем совпадения в файле со словами
+                for (int i = 0; i < arr_words.Length; i++)
+                {
+                    if (tmp.IndexOf(arr_words[i], StringComparison.CurrentCulture) != -1)
+                    {
+                        countConsilience++;
+                    }
+                }
+                if (countConsilience > 0) 
+                {
+                    FileInfo fileInfo = new FileInfo(file); 
+                    //получаем информацию о файле:
+                    //имя
+                    nameFileInfo = fileInfo.Name;
+                    //размер
+                    sizeFileInfo = fileInfo.Length;
+                    //путь
+                    pathFileInfo = fileInfo.FullName;
+                    //количество совпадений  в файле
+                    count = countConsilience;   
+                    
+                    string info = "Имя " + nameFileInfo + " Размер " + sizeFileInfo.ToString() + " Путь " + pathFileInfo.ToString() + " Количество совпадений " + count.ToString();
+                    listInfo.Add(info);
+
+                    //выдает исключение
+                    //File.Copy(pathFileInfo, "C:\\Users\\ADMIN\\Documents\\Игорь Суслов\\Системное программирование\\Exsam", true);
+
+                }
+            }
+        }
+
+        // вывод информации в textbox
+        public void ShowInfo(List<string> listInfo) 
+        { 
+            for (int i = 0;i < listInfo.Count;i++)
+            {
+                textBox3.Text = listInfo[i] + "\r\n";
+            }
+        }
+
+        //Старт приложения
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WordsToSearch();
+            FindFile(textBox2.Text);
+            ShowInfo(listInfo);
+        }
+    }    
+}
